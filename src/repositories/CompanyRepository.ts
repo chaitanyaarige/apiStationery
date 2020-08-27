158519f119274edd09a2eef71805fdf8452d6662
chaitanya <ckrishna@jazeeratech.in>
2020-08-27 18:52:45 +0530

    new backend

--- src/repositories/CompanyRepository.ts
+++ src/repositories/CompanyRepository.ts
@@ -1,21 +1,21 @@
 import { getRepository, Repository } from "typeorm";
-import { Supplier } from "../entity/Supplier";
+import { Company } from "../entity/Company";
 
-export class SupplierRepository {
-  private dao: Repository<Supplier>;
+export class CompanyRepository {
+  private dao: Repository<Company>;
 
   constructor() {
-    this.dao = getRepository(Supplier);
+    this.dao = getRepository(Company);
   }
 
   async search(data: any) {
     return await this.dao
-      .createQueryBuilder("Supplier")
+      .createQueryBuilder("company")
       .where(data)
       .getMany();
   }
 
-  async saveOne(data: Supplier) {
+  async saveOne(data: Company) {
     return await this.dao.save(data);
   }
 
@@ -35,4 +35,4 @@ export class SupplierRepository {
   }
 }
 
-Object.seal(SupplierRepository);
+Object.seal(CompanyRepository);
---
+++ src/services/CompanyService.ts
@@ -0,0 +1,48 @@
+import { Company } from "../entity/Company";
+import { CompanyRepository } from "../repositories/CompanyRepository";
+
+export class CompanyService {
+  public sessionInfo: any;
+  private companyRepository: CompanyRepository;
+  isUpdate: boolean = false;
+
+  constructor() {
+    this.companyRepository = new CompanyRepository();
+  }
+
+  async search(item: any) {
+    try {
+      let data: any = await this.companyRepository.search(item);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findAll() {
+    try {
+      let data: any = await this.companyRepository.findAll();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findOne(id: Company) {
+    try {
+      let data: any = await this.companyRepository.findOne(id);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async saveOne(datas: Company) {
+    try {
+      let data: any = await this.companyRepository.saveOne(datas);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
