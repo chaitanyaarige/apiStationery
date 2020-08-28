28b2a366725245a080fcffdc4a9a563f42d5b97e
chaitanya <ckrishna@jazeeratech.in>
2020-08-28 17:04:46 +0530

    school stationery

---
+++ src/entity/School.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, PrimaryColumn, Column, ManyToOne, JoinColumn, Double, OneToMany, OneToOne, Timestamp } from "typeorm";
+
+@Entity("school")
+export class School {
+  @PrimaryGeneratedColumn({name:"id"})
+  id: number;
+
+  @Column({name:"name"})
+  name: string;
+
+  @Column({name:"address"})
+  address: string;
+
+  @Column({name:"city"})
+  city: string;
+
+  // @Column("int",{array: true, name:"product_category"})
+  // product_category: [];
+
+  // @Column({name: "sizes", type: 'jsonb', nullable: true })
+  // sizes: Object;
+
+  @Column({nullable: true, name:"phone"})
+  phone: number;
+
+  @Column('timestamp')
+  created_at: Timestamp;
+}
